143d505884246fc0ae6f2a9f5e19819346dd2b09
Михаил Кондрашин <misha2kon@mail.ru>
2019-08-30 18:13:19 +0300

    temp commit with PyQT

--- inter.py
+++ inter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'untitled.ui',
-# licensing of 'untitled.ui' applies.
+# Form implementation generated from reading ui file 'deleted file',
+# licensing of 'deleted file' applies.
 #
 # Created: Fri Aug 30 18:01:54 2019
 #      by: pyside2-uic  running on PySide2 5.13.0
@@ -10,6 +10,7 @@
 
 from PySide2 import QtCore, QtGui, QtWidgets
 
+
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
@@ -17,19 +18,19 @@ class Ui_Form(object):
         self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setGeometry(QtCore.QRect(150, 240, 93, 28))
         self.pushButton.setStyleSheet("QPushButton {\n"
-"    background-color: white;\n"
-"    font-size: 14px;\n"
-"    text-align: center;\n"
-"    font-weight: bold;\n"
-"}\n"
-"\n"
-"QPushButton:hover {\n"
-"    background-color: silver;\n"
-"}\n"
-"\n"
-"QPushButton:pressed {\n"
-"    background-color: red;\n"
-"}")
+                                      "    background-color: white;\n"
+                                      "    font-size: 14px;\n"
+                                      "    text-align: center;\n"
+                                      "    font-weight: bold;\n"
+                                      "}\n"
+                                      "\n"
+                                      "QPushButton:hover {\n"
+                                      "    background-color: silver;\n"
+                                      "}\n"
+                                      "\n"
+                                      "QPushButton:pressed {\n"
+                                      "    background-color: red;\n"
+                                      "}")
         self.pushButton.setObjectName("pushButton")
         self.textEdit = QtWidgets.QTextEdit(Form)
         self.textEdit.setGeometry(QtCore.QRect(150, 120, 104, 87))
@@ -41,6 +42,3 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
         self.pushButton.setText(QtWidgets.QApplication.translate("Form", "Тык", None, -1))
-
-
-
