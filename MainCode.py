4008dd42d493167d7e8cd552540e20dc1ad7dd55
Михаил Кондрашин <misha2kon@mail.ru>
2019-08-30 14:21:27 +0300

    second commit (this is temp commit)

--- MainCode.py
+++ MainCode.py
@@ -1,2 +1,15 @@
-import numpy
-print(numpy.zeros(5))
+class MyClass:
+    def __init__(self):
+        self.first = []
+        print('Making')
+
+    def add(self, element):
+        self.first.append(element)
+
+    def get(self, ind):
+        return self.first[ind]
+
+
+mc = MyClass()
+mc.add(1)
+mc.get(0)
