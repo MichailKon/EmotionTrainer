f6415ff20fa5e498308d66f4967d626ec2b2a78e
Михаил Кондрашин <misha2kon@mail.ru>
2019-08-31 12:29:17 +0300

    got video from camera

--- MainCode.py
+++ MainCode.py
@@ -1,23 +1,58 @@
 from PySide2 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QApplication
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
 import sys
 from inter import Ui_Form
+import cv2
+
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QPixmap)
+
+    def __init__(self, parent=None):
+        QThread.__init__(self, parent=parent)
+
+    def run(self):
+                cam = cv2.VideoCapture(0)
+                while cam.isOpened():
+                    ret, frame = cam.read()
+                    if cv2.waitKey(1) & 0xFF == ord(' ') or not ret:
+                        break
+                    rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    convertToQRFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+                    convertToQRFormat = QPixmap.fromImage(convertToQRFormat)
+                    p = convertToQRFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                    self.changePixmap.emit(p)
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        # some inits
+        self.title = 'Image'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
+        # not some inits
+        self.initUI()
+
+    def initUI(self, image=None):
+        self.setWindowTitle('Image')
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(640, 480)
+
+        label = QLabel(self)
+        label.move(0, 0)
+        label.resize(640, 480)
+        th = Thread(self)
+        th.changePixmap.connect(label.setPixmap)
+        th.start()
+        self.show()
 
 
 if __name__ == "__main__":
-    # create app
-    app = QtWidgets.QApplication(sys.argv)
-
-    # create form
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
-
-    # logic
-    def bp():
-        ui.textEdit.setText("Произошел Тык")
-
-    ui.pushButton.clicked.connect(bp)
-
-    # launch
+    app = QApplication(sys.argv)
+    a = App()
     sys.exit(app.exec_())
--- inter.py
+++ inter.py
@@ -1,44 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'deleted file',
-# licensing of 'deleted file' applies.
+# Form implementation generated from reading ui file 'untitled.ui',
+# licensing of 'untitled.ui' applies.
 #
-# Created: Fri Aug 30 18:01:54 2019
+# Created: Sat Aug 31 11:19:55 2019
 #      by: pyside2-uic  running on PySide2 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide2 import QtCore, QtGui, QtWidgets
 
-
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(400, 300)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(150, 240, 93, 28))
-        self.pushButton.setStyleSheet("QPushButton {\n"
-                                      "    background-color: white;\n"
-                                      "    font-size: 14px;\n"
-                                      "    text-align: center;\n"
-                                      "    font-weight: bold;\n"
-                                      "}\n"
-                                      "\n"
-                                      "QPushButton:hover {\n"
-                                      "    background-color: silver;\n"
-                                      "}\n"
-                                      "\n"
-                                      "QPushButton:pressed {\n"
-                                      "    background-color: red;\n"
-                                      "}")
-        self.pushButton.setObjectName("pushButton")
-        self.textEdit = QtWidgets.QTextEdit(Form)
-        self.textEdit.setGeometry(QtCore.QRect(150, 120, 104, 87))
-        self.textEdit.setObjectName("textEdit")
+        Form.resize(1061, 618)
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(110, 60, 800, 480))
+        self.widget.setStyleSheet("QWidget {\n"
+"    alignment: center;\n"
+"}")
+        self.widget.setObjectName("widget")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
-        self.pushButton.setText(QtWidgets.QApplication.translate("Form", "Тык", None, -1))
+
